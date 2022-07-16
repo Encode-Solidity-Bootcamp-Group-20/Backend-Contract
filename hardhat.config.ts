506dbef7cdd2be57c9d388d3f088bc39162c7929
Migcap <angelcap1990@gmail.com>
2022-07-16 16:57:17 -0500

    deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,12 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
 	solidity: "0.8.9",
 	paths: { tests: "tests" },
+	networks: {
+		ropsten: {
+			url: process.env.ROPSTEN_URL || "",
+			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+		},
+	},
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,34 @@
-import { ethers } from "hardhat";
+import { ethers } from "ethers";
+import "dotenv/config";
+import * as NFTCollectionGroup20 from "../artifacts/contracts/NFTCollectionGroup20.sol/NFTCollectionGroup20.json";
+
+// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
+const EXPOSED_KEY = "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+	const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+	console.log(`Using address ${wallet.address}`);
 
-  const lockedAmount = ethers.utils.parseEther("1");
+	const provider = ethers.providers.getDefaultProvider("ropsten");
+	const signer = wallet.connect(provider);
+	const balanceBN = await signer.getBalance();
+	const balance = Number(ethers.utils.formatEther(balanceBN));
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+	if (balance < 0.01) {
+		throw new Error("Not enough ether");
+	}
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+	console.log("Deploying NFT contract...");
+	const NFTFactory = new ethers.ContractFactory(NFTCollectionGroup20.abi, NFTCollectionGroup20.bytecode, signer);
+	const NFTContract = await NFTFactory.deploy();
+	await NFTContract.deployed();
+	console.log("Completed");
+	console.log(`Contract deployed at ${NFTContract.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
